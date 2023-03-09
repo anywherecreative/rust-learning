be69939ad83e248f917d825f77effc311ca5adac
Jeff Manning <info@jeffmanning.ca>
2023-03-09 14:45:05 -0500

    is_some

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    println!("{:?}", get_col_val(1));
-    println!("{:?}", get_col_val(2));
+    for a in 0..5 {
+        let val = get_col_val(a);
+        if val.is_some()  {
+            println!("{} {}",a,val.unwrap())
+        } else {
+            println!("{}",a)
+        }
+    }
 }
 
 fn get_col_val(col: i32 ) -> Option<String> {
